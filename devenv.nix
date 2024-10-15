4b395b76d677b630e3ac355baae482d632bd327c
David Asabina <vid@bina.me>
2024-10-15 23:55:24 +0200

    chore: Update Gleam to unstable version

--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,14 @@
 { pkgs, lib, config, inputs, ... }:
 
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+in
 {
   packages = with pkgs; [
     exercism
   ];
-  languages.gleam.enable = true;
+  languages.gleam = {
+    enable = true;
+    package = pkgs-unstable.gleam;
+  };
 }
